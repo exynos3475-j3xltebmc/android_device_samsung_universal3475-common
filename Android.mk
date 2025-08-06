bff87b154cde0d0f07028777b9a09261893942ee
NotNoelChannel <notnoel12345678@gmail.com>
2025-08-06 15:56:07 +0200

    j3xltebmc: updates

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := device/samsung/universal3475-common
 
-ifneq ($(filter j1xlte, $(TARGET_DEVICE)),)
+ifneq ($(filter j1xlte j3xltebmc, $(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
